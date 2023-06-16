3515f20b491ab692de6fbacc8a3cda5ef48b30c4
Igor <igor1788@gmail.com>
2023-06-16 17:55:34 -0300

    Change api endpoint

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -25,7 +25,7 @@ const Form = () => {
     e.preventDefault();
   
     try {
-      const response = await fetch('http://localhost:3001/cadastro', {
+      const response = await fetch('https://senderbot.onrender.com/cadastro', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
