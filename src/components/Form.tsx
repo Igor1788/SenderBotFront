5e720b9cc451f67ec82b2d5f0dca373df18ed464
Igor Gonçalves <igor1788@gmail.com>
2023-06-27 10:49:38 -0300

    New aendpoint

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -26,7 +26,7 @@ const Form = () => {
     e.preventDefault();
   
     try {
-      const response = await fetch('http://gptconcursos.com.br:3001/cadastro', {
+      const response = await fetch('https://gptconcursos.com.br:3001/cadastro', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
